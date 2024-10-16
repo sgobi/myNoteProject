12d7b6b28ee463229e38299be669cae33ed4c423
sgobi <g2k2@live.com>
2024-10-16 14:40:47 +0530

    add logout and who loging in app deatils show

--- resources/views/notes/layouts/layout.blade.php
+++ resources/views/notes/layouts/layout.blade.php
@@ -6,8 +6,22 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
 </head>
 <body>
-      
+
+
 <div class="container">
+
+      
+<h1>My Notes</h1>
+
+@auth
+    <p>Welcome, {{ auth()->user()->name }}!</p>
+
+    <!-- Logout Form -->
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</button>
+    </form>
+@endauth
     @yield('content')
 </div>
       
